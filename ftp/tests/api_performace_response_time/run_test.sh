96d2eaf63aea690d5f5df9f47a63713a34f9d0d2
Rafael Direito <rdireito@av.it.pt>
2023-06-14 15:20:30 +0100

    Update run_test.sh

--- ftp/tests/api_performace_response_time/run_test.sh
+++ ftp/tests/api_performace_response_time/run_test.sh
@@ -5,6 +5,6 @@
 # @Last Modified time: 2023-06-10 16:29:08
 
 export api_performace_response_time_api_target=https://ci-cd-service.5gasp.eu/dashboard
-export api_performace_response_time_api_target_threshold_ms=1000 # 1000 milliseconds = 1 second
+export api_performace_response_time_threshold_ms=1000 # 1000 milliseconds = 1 second
 python3 -m robot .
 
