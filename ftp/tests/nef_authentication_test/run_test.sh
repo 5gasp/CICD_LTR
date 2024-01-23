2a5bc991cb2afcd62290bb133fca65110d274730
Eduardo Santos <eduardosantoshf@gmail.com>
2024-01-23 15:02:56 +0000

    Documented test api_performance_requests_per_second

--- ftp/tests/nef_authentication_test/run_test.sh
+++ ftp/tests/nef_authentication_test/run_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # @Author: Rafael Direito
 # @Date:   2023-05-22 15:42:35
-# @Last Modified by:   Rafael Direito
-# @Last Modified time: 2024-01-22 16:31:44
+# @Last Modified by:   Eduardo Santos
+# @Last Modified time: 2024-01-23 14:32:46
 
-export nef_authentication_test_reporting_api_ip=10.255.28.236
+export nef_authentication_test_reporting_api_ip=10.255.28.173
 export nef_authentication_test_reporting_api_port=3000
-export nef_authentication_test_mini_api_endpoint_to_invoke=http://10.255.28.230:3001/start/Def19Sec9
+export nef_authentication_test_mini_api_endpoint_to_invoke=http://10.255.28.206:8000/start/Def19Sec9
 python3 -m robot .
