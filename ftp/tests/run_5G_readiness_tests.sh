081f814c47f335b25559e011b4609a35c3f1c5ab
Rafael Direito <rdireito@av.it.pt>
2024-02-04 12:31:13 +0000

    Minor update on the run_5G_readiness_tests.sh script

--- ftp/tests/run_5G_readiness_tests.sh
+++ ftp/tests/run_5G_readiness_tests.sh
@@ -2,7 +2,7 @@
 # @Author: Eduardo Santos
 # @Date:   2024-02-03 18:23:29
 # @Last Modified by:   Rafael Direito
-# @Last Modified time: 2024-02-04 12:02:41
+# @Last Modified time: 2024-02-04 12:21:41
 
 # Save the current working directory
 root_directory="$(pwd)"
@@ -99,6 +99,7 @@ reporting_api_port=3000
 ######################################################
 export mini_api_configuration_configuration_endpoint="$mini_api_server_url/configure"
 export mini_api_configuration_configuration_payload='{"variables":{"NEF_IP":"10.255.28.236","NEF_PORT":8888,"NEF_LOGIN_USERNAME":"admin@my-email.com","NEF_LOGIN_PASSWORD":"pass","SUBS_MONITORING_TYPE":"LOCATION_REPORTING", "SUBS_EXTERNAL_ID": "123456789@domain.com", "SUBS_CALLBACK_URL":"https://webhook.site/43d72330-0f8e-4a52-af1c-65c77d9aafd0","SUBS_MONITORING_EXPIRE_TIME":"2024-03-09T13:18:19.495000+00:00","UE1_NAME":"My UE","UE1_DESCRIPTION":"My UE Description","UE1_IPV4":"10.10.10.10","UE1_IPV6":"0:0:0:0:0:0:0:0","UE1_MAC_ADDRESS":"22-00-00-00-00-02","UE1_SUPI":"202010000000001"}}'
+# mini_api_configuration_configuration_payload - Must be updated (e.g. nef's ip and port, at least)
 run_test "mini_api_configuration"
 
 
@@ -109,6 +110,7 @@ run_test "mini_api_configuration"
 ######################################################
 export mini_api_configuration_configuration_endpoint="$mini_api_ue_url/configure"
 export mini_api_configuration_configuration_payload='{"variables":{"NEF_IP":"10.255.28.236","NEF_PORT":8888,"NEF_LOGIN_USERNAME":"admin@my-email.com","NEF_LOGIN_PASSWORD":"pass","SUBS_MONITORING_TYPE":"LOCATION_REPORTING", "SUBS_EXTERNAL_ID": "123456789@domain.com", "SUBS_CALLBACK_URL":"https://webhook.site/43d72330-0f8e-4a52-af1c-65c77d9aafd0","SUBS_MONITORING_EXPIRE_TIME":"2024-03-09T13:18:19.495000+00:00","UE1_NAME":"My UE","UE1_DESCRIPTION":"My UE Description","UE1_IPV4":"10.10.10.10","UE1_IPV6":"0:0:0:0:0:0:0:0","UE1_MAC_ADDRESS":"22-00-00-00-00-02","UE1_SUPI":"202010000000001"}}'
+# mini_api_configuration_configuration_payload - Must be updated (e.g. nef's ip and port, at least)
 run_test "mini_api_configuration"
 
 
